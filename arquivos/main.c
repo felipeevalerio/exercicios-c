343d60d8a3256186f88423f2c9940517d8aaadc0
felipe-spindola <felipeevalerio@outlook.com>
2021-06-20 21:39:14 -0300

    arquivos-done

--- arquivos/main.c
+++ arquivos/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <math.h>
 
 void exA();
 void exB();
@@ -9,12 +10,13 @@ void exD();
 void exE();
 
 void fibonacci(FILE *);
-int criaMatriz(int [8][8],FILE *);
-void printMatriz(int [8][8]);
+int criaMatriz(float [8][8],FILE *);
+void printMatriz(float [8][8]);
 
 int check();
 
 int main(){
+    setlocale(LC_ALL, "portuguese");
     int ex=1;
     while(ex > 0){
         printf("Digite o número do exercício para poder ver o exercício.\n");
@@ -75,14 +77,14 @@ void exA(){
 }
 
 void fibonacci(FILE *fp){
-
-    int i=3,num,fibo1=1,fibo2=1,soma=0;
-    fprintf(fp,"%d\n",fibo1);
-    fprintf(fp,"%d\n",fibo2);    
+    float fibo1=1,fibo2=1,soma=0;
+    int i = 3;
+    fprintf(fp,"%.0f\n",fibo1);
+    fprintf(fp,"%.0f\n",fibo2);    
 
     for (i; i <= 64; i++) { 
         soma = fibo1 + fibo2;
-        fprintf(fp,"%d\n",soma);                    
+        fprintf(fp,"%.0f\n",soma);                    
         fibo1 = fibo2;                           
         fibo2 = soma;
     }
@@ -92,7 +94,7 @@ void exB(){
     printf("\n\nLer o arquivo letra a e armazenar os dados em uma matriz 8 x 8 imprima essa matriz na tela\n\n");
 
     FILE *fp;
-    int matriz[8][8];
+    float matriz[8][8];
 
     if((fp = fopen("arquivo.txt","r")) == NULL )
         printf("Erro de Abertura\n");
@@ -105,7 +107,115 @@ void exB(){
 
 }
 
-int criaMatriz(int matriz[8][8],FILE *fp){
+
+void exC(){
+    printf("\n\nLer o arquivo letra a e armazenar os dados em uma matriz 8 x 8 armazene essa matriz em um arquivo.\n\n");
+
+    FILE *fp,*newFile;
+    float matriz[8][8];
+
+    if((fp = fopen("arquivo.txt","r")) == NULL )
+        printf("Erro de Abertura\n");
+    else{
+        if((newFile = fopen("matriz.txt","w")) == NULL)
+            printf("Erro de Abertura\n");
+
+        else{
+            matriz[8][8] = criaMatriz(matriz,fp);
+            for(int i= 0; i < 8; i++){
+                for(int j= 0; j < 8; j++){
+                    fprintf(newFile,"%.0f ",matriz[i][j]);
+                }
+                fprintf(newFile,"\n");
+            }
+            fclose(newFile);
+        }
+        fclose(fp);
+
+    }
+
+}
+void exD(){
+    printf("\n\nLer o arquivo letra ae armazenar os valores pares em um arquivo e os impares em outro arquivo.\n\n");
+    
+    FILE *fp,*pares,*impares;
+    char buffer[128];
+
+    
+    if((fp = fopen("arquivo.txt","r")) == NULL)
+        printf("Erro de Abertura\n");
+    else{
+        fgets(buffer,80,fp);    
+        while(!feof(fp)){
+            fgets(buffer,80,fp);    
+            long a = atol(buffer);
+            if(a % 2 == 0){
+                if((pares = fopen("pares.txt","a")) == NULL)
+                    printf("Erro de Abertura\n");
+                else
+                    fprintf(pares,"%ld\n",a);
+            }
+            else{
+                if((impares = fopen("impares.txt","a")) == NULL)
+                    printf("Erro de Abertura\n");
+                else
+                    fprintf(impares,"%ld\n",a);
+            }
+        }
+        printf("Arquivo pares.txt criado com sucesso!\n");
+        printf("Arquivo impares.txt criado com sucesso!\n");
+        fclose(pares);
+        fclose(impares);
+    }
+    fclose(fp);
+}
+
+
+void exE(){
+    printf("\n\nLer o arquivo letra a e armazenar em um vetor apenas os primos, imprima esse vetor, e salve os dados desse vetor em um arquivo.\n\n");
+    
+    FILE * fp;
+    FILE * primos;
+
+    float numPrimo[64], numeros[64];
+    int j=0, moduNum=0, cont=0;
+
+
+    if ((primos = fopen("primos.txt","w"))==NULL) 
+        printf("Erro de abertura!\n");
+    
+    if ((fp = fopen("arquivo.txt","r"))==NULL) 
+        printf("Erro de abertura!\n");
+     
+    else {
+        while (!feof(fp)) {
+            fscanf(fp,"%f",&numeros[j]);
+            for (int i=2; i < numeros[j]; i++) {
+                moduNum=fmod(numeros[j],i);
+                if (moduNum==0) {
+                    numeros[j] = 0;
+                }
+            }
+            j++;
+        }
+        printf("Vetor com primos: ");
+        for (int i=0; i<64; i++) {
+            if (numeros[i] != 0) {
+                numPrimo[cont]=numeros[i];
+                printf("%.0f ",numPrimo[cont]);
+                fprintf(primos,"%.0f\n",numPrimo[cont]);
+                cont++;
+            }
+        }
+        printf("\n");
+        printf("Arquivo primos.txt criado com sucesso!\n");
+        fclose(fp);
+        fclose(primos);
+    }
+        
+}
+
+int criaMatriz(float matriz[8][8],FILE *fp){
     char buffer[128];
 
     fgets(buffer,80,fp);    
@@ -114,7 +224,7 @@ int criaMatriz(int matriz[8][8],FILE *fp){
         for(int i=0;i < 8;i++){
             for(int j=0; j < 8 ; j++){
                 fgets(buffer,80,fp);    
-                matriz[i][j] = atoi(buffer);
+                matriz[i][j] = atof(buffer);
 
             }
         }
@@ -122,10 +232,10 @@ int criaMatriz(int matriz[8][8],FILE *fp){
     return matriz;
 }
 
-void printMatriz(int matriz[8][8]){
+void printMatriz(float matriz[8][8]){
     for(int i=0;i < 8;i++){
         for(int j=0; j < 8 ; j++){
-            printf("\t%d ",matriz[i][j]);
+            printf("\t%.0f ",matriz[i][j]);
 
         }
         printf("\n");
@@ -149,39 +259,4 @@ int check(){
         system("clear");
     #endif
     return choice;
-}
-
-
-void exC(){
-    printf("\n\nLer o arquivo letra a e armazenar os dados em uma matriz 8 x 8 armazene essa matriz em um arquivo.\n\n");
-
-    FILE *fp,*newFile;
-    int matriz[8][8];
-
-    if((fp = fopen("arquivo.txt","r")) == NULL )
-        printf("Erro de Abertura\n");
-    else{
-        if((newFile = fopen("matriz.txt","w")) == NULL)
-            printf("Erro de Abertura\n");
-
-        else{
-            matriz[8][8] = criaMatriz(matriz,fp);
-            for(int i= 0; i < 8; i++){
-                for(int j= 0; j < 8; j++){
-                    fprintf(newFile,"%d ",matriz[i][j]);
-                }
-                fprintf(newFile,"\n");
-            }
-            fclose(newFile);
-        }
-        fclose(fp);
-
-    }
-
-}
-void exD(){
-
-}
-void exE(){
-
 }
